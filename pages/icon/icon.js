7bd35945bfdd3d22ca029d9dd525a693a5bf95f3
ULANE\eshonulane <eshonulane@outlook.com>
2019-05-18 23:38:57 +0800

    icon

---
+++ pages/icon/icon.js
@@ -0,0 +1,146 @@
+// pages/icon/icon.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    icons: [
+      {
+        id: "success",
+        size: 93,
+        title: "成功",
+        cont: "用于表示操作顺利完成"
+      },
+      {
+        id: "info",
+        size: 93,
+        title: "提示",
+        cont: "用于表示信息提示；也常用于缺乏条件的操作拦截，提示用户所需信息"
+      },
+      {
+        id: "warn",
+        size: 93,
+        color: "#C9C9C9",
+        title: "普通警告",
+        cont: "用于表示操作后将引起一定后果的情况；也用于表示由于系统原因而造成的负向结果"
+      },
+      {
+        id: "warn",
+        size: 93,
+        title: "强烈警告",
+        cont: "用于表示由于用户原因造成的负向结果；也用于表示操作后将引起不可挽回的严重后果的情况"
+      },
+      {
+        id: "waiting",
+        size: 93,
+        title: "等待",
+        cont: "用于表示等待，告知用户结果需等待"
+      },
+      {
+        id: "success_no_circle",
+        size: 23,
+        title: "多选控件图标_已选择",
+        cont: "用于多选控件中，表示已选择该项目"
+      },
+      {
+        id: "circle",
+        size: 23,
+        title: "多选控件图标_未选择",
+        cont: "用于多选控件中，表示该项目可被选择，但还未选择"
+      },
+      {
+        id: "warn",
+        size: 23,
+        title: "错误提示",
+        cont: "用于在表单中表示出现错误"
+      },
+      {
+        id: "success",
+        size: 23,
+        title: "单选控件图标_已选择",
+        cont: "用于单选控件中，表示已选择该项目"
+      },
+      {
+        id: "download",
+        size: 23,
+        title: "下载",
+        cont: "用于表示可下载"
+      },
+      {
+        id: "info_circle",
+        size: 23,
+        title: "提示",
+        cont: "用于在表单中表示有信息提示"
+      },
+      {
+        id: "cancel",
+        size: 23,
+        title: "停止或关闭",
+        cont: "用于在表单中，表示关闭或停止"
+      },
+      {
+        id: "search",
+        size: 23,
+        title: "搜索",
+        cont: "用于搜索控件中，表示可搜索"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
